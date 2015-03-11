5d20262d0dfd3e4be4e244a362da947e1ffe1463
Markus Hilbert <m.hilbert@akbild.ac.at>
2015-03-11 13:54:34 +0100

    initial push

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(name="products.goreplace",
+      version="0.0",
+      description="",
+      author="",
+      author_email="",
+      url="",
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      namespace_packages=['products'],
+      packages=find_packages(exclude=['ez_setup']),
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Framework :: Zope2",
+      ],
+      license="GPL2",
+      include_package_data=True,
+      zip_safe=False
+      )
