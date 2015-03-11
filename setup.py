59cceb163ddb1a2724f03ee98615039d96a18e38
Markus Hilbert <m.hilbert@akbild.ac.at>
2015-03-11 14:12:34 +0100

    initial push

--- setup.py
+++ setup.py
@@ -5,23 +5,29 @@
 from setuptools import setup, find_packages
 
 setup(name="products.goreplace",
-      version="0.0",
+      version="1.0",
       description="",
-      author="",
-      author_email="",
-      url="",
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      namespace_packages=['products'],
-      packages=find_packages(exclude=['ez_setup']),
       classifiers=[
           "Framework :: Plone",
           "Programming Language :: Python",
           "Framework :: Zope2",
       ],
+      keywords='akbild plone goreplace',
+      author="Markus Hilbert, Georg Bernhard",
+      author_email="m.hilbert@akbild.ac.at, gogo@akbild.ac.at",
+      url="https://www.akbild.ac.at",
       license="GPL2",
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['products'],
       include_package_data=True,
-      zip_safe=False
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
       )
